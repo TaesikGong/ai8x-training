3a48ffe94339d24f009fee3781bfbd555ed360c9
TaesikGong <cathena913@gmail.com>
2024-03-22 15:35:37 +0000

    Add quantization experiment scripts for Synergy motiv section

---
+++ scripts/custom/train_cifar100_qat8_mobilenet_v2_0.75_16bit.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# same as the original train_cifar100_qat8_mobilenet_v2_0.75_fullbit.sh, but added CUDA_VISIBLE_DEVICES.
+CUDA_VISIBLE_DEVICES=1 python train.py --deterministic --epochs 300 --optimizer SGD --lr 0.1 --compress policies/schedule-cifar100-mobilenetv2.yaml --model ai87netmobilenetv2cifar100_m0_75 --dataset CIFAR100 --device MAX78002 --batch-size 128 --print-freq 100 --validation-split 0 --use-bias --qat-policy policies/custom/qat_policy_cifar100_mobilenetv2_16bit.yaml "$@"
